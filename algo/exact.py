8098e78c7a9276d3ae0775e698d4c5af492ca7ea
anirbanl <anirbanlaha@gmail.com>
2020-04-08 17:28:06 -0400

    added util functiontable

--- algo/exact.py
+++ algo/exact.py
@@ -13,47 +13,50 @@ from jax.experimental import optimizers
 from jax.tree_util import tree_multimap  # Element-wise manipulation of collections of numpy arrays
 from jax.ops import index, index_add, index_update
 import os, time
+from lib import util
 
 rng=jax.random.PRNGKey(1234)
 
-def naive(Ls, th, **hp):
-    grad_L = jacobian(Ls)(th) # n x n x d
-    grads = jp.einsum('iij->ij', grad_L)
-    step = hp['eta'] * grads
-    return th - step.reshape(th.shape), Ls(th)
+@util.functiontable
+class Algorithms:
+    def naive(Ls, th, **hp):
+        grad_L = jacobian(Ls)(th) # n x n x d
+        grads = jp.einsum('iij->ij', grad_L)
+        step = hp['eta'] * grads
+        return th - step.reshape(th.shape), Ls(th)
 
-def la(Ls, th, **hp):
-    grad_L = jacobian(Ls)(th) # n x n x d
-    def fn1(th):
-        xi = jp.lax.stop_gradient(jp.einsum('ii...->i...', jax.jacrev(Ls)(th)))
-        _, prod = jax.jvp(Ls, (th,), (xi,))
-        return (prod - jp.einsum('ij,ij->i', xi, xi))
+    def la(Ls, th, **hp):
+        grad_L = jacobian(Ls)(th) # n x n x d
+        def fn1(th):
+            xi = jp.lax.stop_gradient(jp.einsum('ii...->i...', jax.jacrev(Ls)(th)))
+            _, prod = jax.jvp(Ls, (th,), (xi,))
+            return (prod - jp.einsum('ij,ij->i', xi, xi))
 
-    xi = jp.einsum('iij->ij', grad_L)
-    grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
-    step = hp['eta'] * grads
-    return th - step.reshape(th.shape), Ls(th)
+        xi = jp.einsum('iij->ij', grad_L)
+        grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
+        step = hp['eta'] * grads
+        return th - step.reshape(th.shape), Ls(th)
 
-def lola(Ls, th, **hp):
-    grad_L = jacobian(Ls)(th) # n x n x d
-    def fn1(th):
-        xi = jp.einsum('ii...->i...', jax.jacrev(Ls)(th))
-        _, prod = jax.jvp(Ls, (th,), (xi,))
-        return (prod - jp.einsum('ij,ij->i', xi, xi))
+    def lola(Ls, th, **hp):
+        grad_L = jacobian(Ls)(th) # n x n x d
+        def fn1(th):
+            xi = jp.einsum('ii...->i...', jax.jacrev(Ls)(th))
+            _, prod = jax.jvp(Ls, (th,), (xi,))
+            return (prod - jp.einsum('ij,ij->i', xi, xi))
 
-    xi = jp.einsum('iij->ij', grad_L)
-    grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
-    step = hp['eta'] * grads
-    return th - step.reshape(th.shape), Ls(th)
+        xi = jp.einsum('iij->ij', grad_L)
+        grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
+        step = hp['eta'] * grads
+        return th - step.reshape(th.shape), Ls(th)
 
-def symlola(Ls, th, **hp):
-    grad_L = jacobian(Ls)(th) # n x n x d
-    def fn1(th):
-        xi = jp.einsum('ii...->i...', jax.jacrev(Ls)(th))
-        _, prod = jax.jvp(Ls, (th,), (xi,))
-        return prod
+    def symlola(Ls, th, **hp):
+        grad_L = jacobian(Ls)(th) # n x n x d
+        def fn1(th):
+            xi = jp.einsum('ii...->i...', jax.jacrev(Ls)(th))
+            _, prod = jax.jvp(Ls, (th,), (xi,))
+            return prod
 
-    xi = jp.einsum('iij->ij', grad_L)
-    grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
-    step = hp['eta'] * grads
-    return th - step.reshape(th.shape), Ls(th)
+        xi = jp.einsum('iij->ij', grad_L)
+        grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
+        step = hp['eta'] * grads
+        return th - step.reshape(th.shape), Ls(th)
