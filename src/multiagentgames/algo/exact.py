c0b4e56533e8dbe01f3034dfbc6e53e6bf282cb7
anirbanl <anirbanlaha@gmail.com>
2020-04-09 01:55:44 -0400

    Added SOS exact algorithm

--- src/multiagentgames/algo/exact.py
+++ src/multiagentgames/algo/exact.py
@@ -1,4 +1,4 @@
-from jax import jacobian
+from jax import jacobian, lax
 import jax
 import jax.numpy as jp
 from jax.ops import index, index_add, index_update
@@ -59,5 +59,32 @@ class Algorithms:
 
         xi = jp.einsum('iij->ij', grad_L)
         grads = xi - hp['alpha'] * jp.einsum('ii...->i...', jax.jacrev(fn1)(th))
+        step = hp['eta'] * grads
+        return th - step.reshape(th.shape), Ls(th)
+
+    def sos(Ls, th, hp):
+        grad_L = jacobian(Ls)(th) # n x n x d
+        xi = jp.einsum('iij->ij',grad_L)
+        full_hessian = jax.hessian(Ls)(th)
+        off_diag_hessian = full_hessian
+        for i in range(th.shape[0]):
+            off_diag_hessian = index_update(off_diag_hessian, index[i,i,:,:,:], 0)
+        second_term = - hp['alpha'] * jp.einsum('iim->im',jp.einsum('ijklm,jk->ilm', off_diag_hessian, xi))
+        xi_0 = xi + second_term # n x d
+        diag_hessian = jp.einsum('iijkl->ijkl', full_hessian)
+        for i in range(th.shape[0]):
+            diag_hessian = index_update(diag_hessian, index[i,:,i,:], 0)
+        third_term = - hp['alpha'] * jp.einsum('iij->ij',jp.einsum('ijkl,mij->mkl',diag_hessian,grad_L))
+        dot = jp.einsum('ij,ij', third_term, xi_0)
+        pass_through = lambda x: x
+        p1 = lax.cond(dot >= 0, #Condition
+                      1.0, pass_through, #True
+                      jp.minimum(1, - hp['a'] * jp.linalg.norm(xi_0)**2 / dot), pass_through) #False
+        xi_norm = jp.linalg.norm(xi)
+        p2 = lax.cond(xi_norm < hp['b'], #Condition
+                      xi_norm**2, pass_through, #True
+                      1.0, pass_through) #False
+        p = jp.minimum(p1, p2)
+        grads = xi_0 + p * third_term
         step = hp['eta'] * grads
         return th - step.reshape(th.shape), Ls(th)
