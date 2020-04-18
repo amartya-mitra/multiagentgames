68fe25c20c4283a8ae59b7d10ccda86609f965f4
anirbanl <anirbanlaha@gmail.com>
2020-04-18 00:57:58 -0400

    Added interpolated SGA exact algo for Tandem

--- src/multiagentgames/expt/tandem_exact.py
+++ src/multiagentgames/expt/tandem_exact.py
@@ -21,7 +21,7 @@ def main():
     num_epochs = 30
     hp = {'eta': 0.1, 'alpha': 0.1, 'a': 0.5, 'b': 0.5, 'lambda':1.0}
     std = 0.1
-    algo_list = ['NAIVE', 'LOLA0', 'LOLA', 'LA', 'SYMLOLA', 'SOS', 'SGA', 'CO', 'EG', 'CGD', 'LSS'][0:7]
+    algo_list = ['NAIVE', 'LOLA0', 'LOLA', 'LA', 'SYMLOLA', 'SOS', 'SGA', 'PSGA', 'CO', 'EG', 'CGD', 'LSS'][0:8]
     theta = vmap(partial(init_th, dims, std))(jax.random.split(rng, num_runs))
 
     t1 = time.time()
